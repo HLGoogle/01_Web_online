e50cc48b4475c374d473ab9ba997cfc8d51d9614
HLGoogle <hl273573221@gmail.com>
2025-01-22 14:31:28 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// GitHub 配置
+const config = {
+    GITHUB_TOKEN: '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',
+    REPO_OWNER: 'HLGoogle',
+    REPO_NAME: '01_Web_online',
+    FILE_PATH: 'sections.sql'
+};
