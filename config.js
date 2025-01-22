e79da5531a39ecdd75e61e482b858a81cee7c63b
HLGoogle <hl273573221@gmail.com>
2025-01-22 14:35:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // GitHub 配置
 const config = {
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: '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',
     REPO_OWNER: 'HLGoogle',
     REPO_NAME: '01_Web_online',
     FILE_PATH: 'sections.sql'
