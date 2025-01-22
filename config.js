42ea8867dd900f0e5a23a85b4e3160f551d90cc9
HLGoogle <hl273573221@gmail.com>
2025-01-22 16:22:21 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// GitHub 配置
-const config = {
-    GITHUB_TOKEN: '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',
-    REPO_OWNER: 'HLGoogle',
-    REPO_NAME: '01_Web_online',
-    FILE_PATH: 'sections.sql'
-};
