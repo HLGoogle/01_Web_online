e14d36e29ac3005cde3c61475a66ed1a028e1d1c
HLGoogle <hl273573221@gmail.com>
2025-01-22 15:10:51 +0800

    更新内容

--- sections.sql
+++ sections.sql
@@ -107,3 +107,4 @@ INSERT INTO sections (title, content, type) VALUES
 
 INSERT INTO sections (title, content, type) VALUES ('11', '11', '类型1');
 INSERT INTO sections (title, content, type) VALUES ('111', '分分分', '类型1');
+INSERT INTO sections (title, content, type) VALUES ('11', '222', '类型1');
