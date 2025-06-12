74cbe68f51ca4f3ee5535ebf0bfc5b965605c93a
HLGoogle <hl273573221@gmail.com>
2025-06-12 13:28:41 +0800

    Update content.js

--- content.js
+++ content.js
@@ -79,17 +79,18 @@ window.linksData = [
         comment: "宣传",
         note: "账号：hwx182980  密码：hwx182980"
     },
-    {
-        type: "宣传",
-        url: "www.55nets.com",
-        icon: "55",
-        comment: "宣传",
-        note: "账号：hl**6 密码：27**1   "    },
+
     {
         type: "宣传",
         url: "www.shema.cc/forum.php?mod=forumdisplay&fid=37",
         icon: "社马",
         comment: "宣传",
         note: "账号：hwx182980  密码：hwx182980"
-    }
+    },
+        {
+        type: "宣传",
+        url: "www.55nets.com",
+        icon: "55",
+        comment: "宣传",
+        note: "账号：hl**6 密码：27**1   "    },
 ];
